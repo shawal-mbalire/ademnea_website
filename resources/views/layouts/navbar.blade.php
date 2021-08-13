4cddf99ae6f1954441d6f1bc5f28e0638c8c73ba
PaulHouseLumu <paulhouselumu@gmail.com>
2021-08-13 17:11:13 +0300

    Good day message 7

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -18,7 +18,7 @@
     <div class="navbar-menu-wrapper navbar-dark d-flex align-items-top" style="background:green;">
       <ul class="navbar-nav">
         <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-          <h1 class="welcome-text text-light">Good Morning, <span class="text-black fw-bold text-light-100">{{Auth::user()->name}}</span></h1>
+          <h1 class="welcome-text text-light" id="greetings"></h1>
           <h3 class="welcome-sub-text"></h3>
         </li>
       </ul>

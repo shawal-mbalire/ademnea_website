59807c5b7d6054017c65b182711cde19429a1905
nsengiyumva-wilberforce <nsengiyumvawilberforce@gmail.com>
2021-08-13 16:19:01 +0300

    CSS changes by wilber

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,24 +1,24 @@
 <!-- partial:partials/_navbar.html -->
 <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
-    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start bg-dark">
       <div class="me-3">
-        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-bs-toggle="minimize">
+        <button class="navbar-toggler bg-light navbar-toggler align-self-center" type="button" data-bs-toggle="minimize">
           <span class="icon-menu"></span>
         </button>
       </div>
-      <div>
+      <div class="bg-light">
         <a class="navbar-brand brand-logo" href="/admin/team">
-          <img src="{{asset('dash/logo2.png')}}" alt="logo" />
+          <img src="{{asset('dash/logo2.png')}}" alt="logo" width="300" height="400">
         </a>
         <a class="navbar-brand brand-logo-mini" href="/admin/team">
           <img src="{{asset('dash/icon.png')}}" alt="logo" />
         </a>
       </div>
     </div>
-    <div class="navbar-menu-wrapper d-flex align-items-top">
+    <div class="navbar-menu-wrapper navbar-dark d-flex align-items-top" style="background:green;">
       <ul class="navbar-nav">
         <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-          <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold">{{Auth::user()->name}}</span></h1>
+          <h1 class="welcome-text text-light">Good Morning, <span class="text-black fw-bold text-light-100">{{Auth::user()->name}}</span></h1>
           <h3 class="welcome-sub-text"></h3>
         </li>
       </ul>
@@ -155,7 +155,7 @@
         </li> --}}
         <li class="nav-item dropdown d-none d-lg-block user-dropdown">
           <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-            <span class="text-black fw-bold">{{Auth::user()->name}}</span>
+            <span class="text-light fw-bold">{{Auth::user()->name}}</span>
           <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
             <div class="dropdown-header text-center">
               <img class="img-md rounded-circle" src="images/faces/face8.jpg" alt="Profile image">

b3f13c8c0bcf32fa5e1ecb158a5c6be7d16dd8b4
PaulHouseLumu <paulhouselumu@gmail.com>
2021-08-17 00:45:05 +0300

    Changed appearance of Login Page to adapt to AdEMNEA theme-color(green) By: Lumu Paul Elijah

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,125 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
-<!-- Mirrored from demo.dashboardpack.com/admindek-html/default/auth-sign-in-social.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 Mar 2021 02:03:16 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
-    <title>AdEMNEA</title>
-    <!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 10]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-      <![endif]-->
-      <!-- Meta -->
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="description" content="Admindek Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
-      <meta name="keywords" content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
-      <meta name="author" content="colorlib" />
-      <!-- Favicon icon -->
-
-      <link rel="icon" href="{{asset('/files/assets/images/favicon.ico')}}" type="image/x-icon">
-      <!-- Google font-->
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
-      <!-- Required Fremwork -->
-      <link rel="stylesheet" type="text/css" href="{{asset('files/bower_components/bootstrap/css/bootstrap.min.css')}}">
-      <!-- waves.css -->
-      <link rel="stylesheet" href="{{asset('files/assets/pages/waves/css/waves.min.css')}}" type="text/css" media="all"><!-- feather icon --> <link rel="stylesheet" type="text/css" href="/files/assets/icon/feather/css/feather.css">
-      <!-- themify-icons line icon -->
-      <link rel="stylesheet" type="text/css" href="{{asset('files/assets/icon/themify-icons/themify-icons.css')}}">
-      <!-- ico font -->
-      <link rel="stylesheet" type="text/css" href="{{asset('files/assets/icon/icofont/css/icofont.css')}}">
-      <!-- Font Awesome -->
-      <link rel="stylesheet" type="text/css" href="{{asset('files/assets/icon/font-awesome/css/font-awesome.min.css')}}">
-      <!-- Style.css -->
-      <link rel="stylesheet" type="text/css" href="{{asset('files/assets/css/style.css')}}">
-      <link rel="stylesheet" type="text/css" href="{{asset('/files/assets/css/pages.css')}}">
-  </head>
-
-  <body  style="background-image: url('{{asset('files/bee.jpg')}}');background-repeat: no-repeat;
-  background-size: 1920px 1024px;">
-    <section class="login-block">
-        <!-- Container-fluid starts -->
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-12">
-                    <!-- Authentication card start -->
-                    <form class="" method="POST" action="{{route('auth.login')}}">
+    <title>AdEMNEA|Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ asset('dash/AdEMNEA_logo.png') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/fonts/iconic/css/material-design-iconic-font.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/animsition/css/animsition.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/util_log.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style_log.css') }}">
+    <!--===============================================================================================-->
+</head>
+<body  style="background-image: url('{{asset('files/bee.jpg')}}');background-repeat: no-repeat;background-size: 1920px 1024px;">
+<section class="login-block">
+    <div class="limiter">
+        <div class="container-login100" >
+            <div class="wrap-login100">
+                <form class="login100-form validate-form" method="POST" action="{{route('auth.login')}}" >
                     @csrf
-                    <div class="text-center">
-                        {{-- <h2>AdEMNEA</h2> --}}
-                        <img src="{{asset('dash/logo.png')}}"  alt="AdEMNEA">
-                    </div>
-                        <div class="auth-box card">
-                            <div class="card-block">
-                                <div class="row m-b-20">
-                                    <div class="col-md-12">
-                                        <h3 class="text-center txt-primary">Sign In</h3>
-                                    </div>
-                                    <div class="col-md-12">
-                                        @if($errors->any())
-                                        @foreach ($errors->all() as $error)
-                                        <div class="alert alert-danger background-danger">
-                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span class="login100-form-logo">
+						<style>
+                          img {
+                              border-radius: 20%;
+                          }
+                        </style>
+						<a href="/" ><img src="{{ asset('dash/AdEMNEA_logo.png' ) }}" alt="AdEMNEA_logo" width="100" height="30"></a>
+					</span>
+
+                    <span class="login100-form-title p-b-34 p-t-27">
+						Log in
+					</span>
+                    <div class="row m-b-20">
+                        <div class="col-md-12">
+                            @if($errors->any())
+                                @foreach ($errors->all() as $error)
+                                    <div class="alert alert-danger background-danger">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                             <i class="icofont icofont-close-line-circled text-white"></i>
-                                            </button>
-                                            <strong>{{$error}}</strong>
-                                        </div>
-                                        @endforeach
-                                        @endif
+                                        </button>
+                                        <strong>{{$error}}</strong>
                                     </div>
-                                </div>
-                                <div class="form-group form-primary">
-                                    <input type="text" name="email" class="form-control" required="">
-                                    <span class="form-bar"></span>
-                                    <label class="float-label">Email</label>
-                                </div>
-                                <div class="form-group form-primary">
-                                    <input type="password" name="password" class="form-control" required="">
-                                    <span class="form-bar"></span>
-                                    <label class="float-label">Password</label>
-                                </div>
-
-                                <div class="row m-t-30">
-                                    <div class="col-md-12">
-                                        <button type="submit" class="btn btn-success btn-md btn-block waves-effect text-center m-b-20" >LOGIN</button>
-
-                                    </div>
-                                </div>
-                                <div class="row m-t-25 text-left">
-                                    <div class="col-12">
-                                        <div class="forgot-phone text-right float-right">
-                                            <a href="/register" class="text-right f-w-600"> I dont have an account</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                                @endforeach
+                            @endif
                         </div>
-                    </form>
-                        <!-- end of form -->
                     </div>
-                    <!-- Authentication card end -->
-                </div>
-                <!-- end of col-sm-12 -->
-            </div>
-            <!-- end of row -->
-        <!-- end of container-fluid -->
-    </section>
+                    <div class="wrap-input100 validate-input" data-validate = "Enter email">
+                        <input class="input100" type="text" name="email" placeholder="Email" required>
+                        <span class="focus-input100" data-placeholder="&#xf207;"></span>
+                    </div>
 
-<script type="text/javascript" src="{{asset('/files/bower_components/jquery/js/jquery.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('/files/bower_components/jquery-ui/js/jquery-ui.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('/files/bower_components/popper.js/js/popper.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('/files/bower_components/bootstrap/js/bootstrap.min.js')}}"></script>
-<!-- waves js -->
-<script src="..{{asset('/files/assets/pages/waves/js/waves.min.js')}}"></script>
-<!-- jquery slimscroll js -->
-<script type="text/javascript" src="{{asset('/files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js')}}"></script>
-<!-- modernizr js -->
-<script type="text/javascript" src="{{asset('/files/bower_components/modernizr/js/modernizr.js')}}"></script>
-<script type="text/javascript" src="{{asset('/files/bower_components/modernizr/js/css-scrollbars.js')}}"></script>
-<script type="text/javascript" src="{{asset('/files/assets/js/common-pages.js')}}"></script>
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <input class="input100" type="password" name="password" placeholder="Password" required>
+                        <span class="focus-input100" data-placeholder="&#xf191;"></span>
+                    </div>
+
+                    <div class="contact100-form-checkbox">
+                        <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                        <label class="label-checkbox100" for="ckb1">
+                            Remember me
+                        </label>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!--===============================================================================================-->
+<script src="{{ asset('assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('assets/vendor/animsition/js/animsition.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('assets/vendor/bootstrap/js/popper.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('assets/vendor/select2/select2.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('assets/vendor/daterangepicker/moment.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('assets/vendor/countdowntime/countdowntime.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
 </body>
 </html>

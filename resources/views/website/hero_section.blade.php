891e481fb3c774bc16d6a3925521c4577ec68b81
Dancer3 <katambaj90@gmail.com>
2021-08-17 04:25:12 -0700

    workpackages

--- resources/views/website/hero_section.blade.php
+++ resources/views/website/hero_section.blade.php
@@ -5,24 +5,51 @@
 
         <div class="carousel-inner" role="listbox">
 
-            @foreach ($workpackages as $workpackage)
-    
             <!-- Slide 1 -->
-            <div class="carousel-item active">
+            <div class="carousel-item active" style="background-image: url(assets/img/slide/bee.jpg)">
                 <div class="carousel-container">
                     <div class="container">
-                        <h2 class="animate__animated animate__fadeInDown">{{ $workpackage->name }}</h2>
+                        <h2 class="animate__animated animate__fadeInDown">Sensors and signal processing </h2>
                         <p class="animate__animated animate__fadeInUp">
-                            {{ $workpackage->description }}
+                            Design and implementation of sensors, supporting electronics and data processing methods for: automatic real-time identification, counting and tracking of  insects that are key agents in the pollination process (honey bees, solitary bees, bumble bees, carpenter bees, hoverflies, butterflies) or crop pests (ants, beetles, caterpillars, army worm, mites, locusts, aphids).
                         </p>
                         <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read
                             More</a>
                     </div>
                 </div>
             </div>
-            @endforeach
 
-            
+            <!-- Slide 2 -->
+            <div class="carousel-item" style="background-image: url(assets/img/slide/sensors.jpg)">
+                <div class="carousel-container">
+                    <div class="container">
+                        <h2 class="animate__animated animate__fadeInDown">Networks and Resilience</h2>
+                        <p class="animate__animated animate__fadeInUp">Design and implementation of resilient networks
+                            for environmental monitoring and network operation models addressing the specific challenges
+                            in the regions involved, in particular harsh environment conditions and vandalism.</p>
+                        <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read
+                            More</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Slide 3 -->
+            <div class="carousel-item" style="background-image: url(assets/img/slide/analytics.jpg)">
+                <div class="carousel-container">
+                    <div class="container">
+                        <h2 class="animate__animated animate__fadeInDown">Data analytics for Insect pollinator
+                            monitoring </h2>
+                        <p class="animate__animated animate__fadeInUp">This work focuses on documenting the insect
+                            pollinator species diversity and abundance across different agricultural landscapes. As well
+                            as their health status all aimed at contributing towards their conservation through
+                            identification of biodiversity hotspots in the region. Insect pollinators such as bees are
+                            very important in the East African cropping system since them pollinator crops and generate
+                            incomes through their product sales.</p>
+                        <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read
+                            More</a>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">

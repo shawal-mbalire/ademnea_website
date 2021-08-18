2ce468cf189836d631907690f2b24336a58e2277
PaulHouseLumu <paulhouselumu@gmail.com>
2021-08-18 17:01:46 +0300

    Gallery Section Edition 2 By: Lumu Paul Elijah

--- resources/views/website/gallery.blade.php
+++ resources/views/website/gallery.blade.php
@@ -6,6 +6,34 @@
 }
 </style>
 
+<script>
+    $(document).ready(function () {
+
+        $("#owl-demo").owlCarousel({
+
+            autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+            items: 4,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [979, 3]
+
+        });
+
+    });
+</script>
+
+{{--<style>--}}
+{{--    #owl-demo .item {--}}
+{{--        margin: 3px;--}}
+{{--    }--}}
+
+{{--    #owl-demo .item img {--}}
+{{--        display: block;--}}
+{{--        width: 100%;--}}
+{{--        height: auto;--}}
+{{--    }--}}
+{{--</style>--}}
+
 <section id="gallery" class="gallery">
     <div class="container">
 
@@ -14,48 +42,34 @@
         </div>
         @if($gallery->count())
             <div class="row">
+                <div class="clients-slider swiper-container">
+                    <div class="swiper-wrapper align-items-center">
                 @foreach($gallery as $item)
                     <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-                       <div class="carousel-container">
-                        <div class="member">
-                            <style>
-                                img {
-                                    border-radius: 8px;
-                                }
-                            </style>
-                          <figure>
-                              <img src="{{asset('gallery/' .$item->image_url)}}" alt="No image">
-                            <figcaption style="text-align:center; font-color:grey; font-style:italic">{{$item->title}}
-                            </figcaption>
-                          </figure>
-                        </div>
-                       </div>
+
+
+                                <div class="member">
+                                    <style>
+                                        img {
+                                            border-radius: 8px;
+                                        }
+                                    </style>
+                                    <figure><div id="owl-demo">
+                                        <img src="{{asset('gallery/' .$item->image_url)}}" alt="No image">
+                                        <figcaption
+                                            style="text-align:center; font-color:grey; font-style:italic">{{$item->title}}
+                                        </figcaption> </div>
+                                    </figure>
+
+                            </div>
                     </div>
                 @endforeach
+                    </div>
+                </div>
                 @else
                     <p style="text-align:center; font-style: italic; color:grey;">[Gallery will be updated soon!]</p>
                 @endif
 
             </div>
-        </div>
-
-{{--        @foreach($gallery as $item)--}}
-{{--        <div>--}}
-{{--            {{$item -> title}}--}}
-{{--        </div>--}}
-
-{{--        <div>--}}
-{{--        <img src="{{asset('images/' . $item->image)}}" alt="" id="displayImage">--}}
-{{--        </div>--}}
-
-{{--        <div>--}}
-{{--        {{$item -> description}}--}}
-{{--        </div>--}}
-{{--        <br>--}}
-{{--        <br>--}}
-
-{{--        @endforeach--}}
-{{--        </div>--}}
-
     </div>
 </section>

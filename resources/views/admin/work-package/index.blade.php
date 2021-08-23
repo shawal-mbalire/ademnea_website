01cac66fdec0e13dccbb3fd0f26178481b1ea909
nsengiyumva-wilberforce <nsengiyumvawilberforce@gmail.com>
2021-08-23 16:35:40 +0300

    changes

--- resources/views/admin/work-package/index.blade.php
+++ resources/views/admin/work-package/index.blade.php
@@ -8,7 +8,6 @@
 
 
     }
-
     .card-header{
         background-color: none;
     }
@@ -47,13 +46,14 @@
                                 @foreach($workpackage as $item)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->name }}</td><td>{{ $item->abbreviation }}</td><td>{{ $item->description }}</td>
+                                        <td>{{ $item->name }}</td>
+                                         <td>{{ $item->abbreviation }}</td>
+                                        <td>{{ $item->description }}</td><td>
                                             <a href="{{ url('/admin/work-package/' . $item->id) }}" title="View WorkPackage"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
                                             <a href="{{ url('/admin/work-package/' . $item->id . '/edit') }}" title="Edit WorkPackage"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
                                             <a href="{{ url('/admin/tasks/create') }}" class="btn btn-success btn-sm" title="Attach Activity">
                                                 <i class="fa fa-plus" aria-hidden="true"></i> Add Task
                                             </a>
-
                                             <form method="POST" action="{{ url('/admin/work-package' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                                 {{ method_field('DELETE') }}
                                                 {{ csrf_field() }}

fc2c9c45da6230ec64e5b43906c8ef697633727a
nsengiyumvawilberforce@gmail.com <nsengiyumvawilberforce@gmail.com>
2022-04-03 08:39:40 +0300

    image height

--- resources/views/website/gallerylayout.blade.php
+++ resources/views/website/gallerylayout.blade.php
@@ -36,9 +36,9 @@
   }
   .d-block w-100 {
    width:100%;
-   height:550px;
+   height:4500px;
    object-fit:cover;
-   max-height: 200px;
+   max-height: 490px;
    object-position:50% 50%;
   }
       </style>

523cb9a90b87c62718f8232fd1aff08f564e9520
nsengiyumvawilberforce@gmail.com <nsengiyumvawilberforce@gmail.com>
2022-05-13 21:10:46 +0300

    newsletters table resolved

---
+++ database/migrations/2022_05_13_140823_drop_newsletters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropNewslettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('newsletters', function (Blueprint $table) { 
+        $table->increments('id');
+            $table->timestamps();
+            $table->string('title')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('article')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}

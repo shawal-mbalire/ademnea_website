70ad7f2f5e97070f30a62f752a49ce79d78f43d2
Benjamin Rukundo <rukundob451@gmail.com>
2022-09-29 00:12:08 +0300

    ignore

--- resources/views/website/header.blade.php
+++ resources/views/website/header.blade.php
@@ -17,8 +17,8 @@
                         <li><a href="/workpackages-wp3">Data Analytics for Environment Monitoring services(WP3)</a></li>
                         <li><a href="/workpackages-wp4">Project Administration(WP4)</a></li>
                     </ul>
-
                 </li>
+                
                 <li class="dropdown scrollto dropdown"><a href="/" class="nav-link scrollto"><span>Research
                             Profile</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>

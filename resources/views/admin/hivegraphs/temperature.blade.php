639d15a1f076463e576b5661190f0a53287892a1
achuka99 <achukasimon1@gmail.com>
2023-07-04 11:23:21 +0300

    new navbar

--- resources/views/admin/hivegraphs/temperature.blade.php
+++ resources/views/admin/hivegraphs/temperature.blade.php
@@ -11,8 +11,20 @@
 </head>
 <body>
 
+<div>
+<a href="{{ url('admin/hive') }}" class="inline-block px-2 py-2 rounded-sm text-white bg-blue-700 hover:bg-blue-500">Back</a>
+</div>
 
- <div class="flex flex-row space-x-4 mt-10 items-center justify-center h-16 mb-4 rounded bg-gray-200 dark:bg-gray-800">
+
+ <div class="flex flex-row space-x-2 mt-5 items-center justify-between h-16 mb-2 rounded bg-gray-200 dark:bg-gray-800">
+ <div>
+        <h2 class="text-xl ml-3 font-bold leading-none tracking-tight text-black md:text-2xl lg:text-xl dark:text-black">
+            Farm: <span class="font-extrabold">{{ $farm_name }}</span>
+        </h2>
+        <h2 class="text-xl ml-3 font-bold leading-none tracking-tight text-black md:text-2xl lg:text-xl dark:text-black mt-2">
+            Hive  : <span class="font-extrabold">{{ $hive_id }}</span>
+        </h2>
+      </div>
          
          <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Graphs <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
 <!-- Dropdown menu -->
@@ -68,87 +80,52 @@
     </ul>
 </div>
 
+<div>
+        <h3 class='mx-2 font-bold py-1'>Select a date-range</h3>
+              <!-- Date range picker -->
+            <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 250px">
+                <i class="fa fa-calendar"></i>&nbsp;
+                <span></span> <i class="fa fa-caret-down"></i>
+            </div>
+      </div>
 
+            <script type="text/javascript">
+            $(function() {
+
+                var start = moment().subtract(29, 'days');
+                var end = moment();
+
+                function cb(start, end) {
+                    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+                }
+
+                $('#reportrange').daterangepicker({
+                    startDate: start,
+                    endDate: end,
+                    ranges: {
+                      'Today': [moment(), moment()],
+                      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                      'This Month': [moment().startOf('month'), moment().endOf('month')],
+                      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                    }
+                }, cb);
+
+                cb(start, end);
+
+            });
+</script>
   </div>
+
+
  
 
 
-<!-- Date range picker -->
- <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
-    <i class="fa fa-calendar"></i>&nbsp;
-    <span></span> <i class="fa fa-caret-down"></i>
-</div>
-
-<script type="text/javascript">
-$(function() {
-
-    var start = moment().subtract(29, 'days');
-    var end = moment();
-
-    function cb(start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
-
-    $('#reportrange').daterangepicker({
-        startDate: start,
-        endDate: end,
-        ranges: {
-           'Today': [moment(), moment()],
-           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-           'This Month': [moment().startOf('month'), moment().endOf('month')],
-           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        }
-    }, cb);
-
-    cb(start, end);
-
-});
-</script>
 </body>
 
     
-  <div class='flex flex-row mx-3 space-x-4  items-center justify-between bg-red-200 mt-2'>
-      <div>
-        <h1 class="text-xl ml-3 font-bold leading-none tracking-tight text-gray-200 md:text-2xl lg:text-xl dark:text-black">
-            Farm: <span class="font-extrabold">{{ $farm_name }}</span>
-        </h1>
-        <h1 class="text-xl ml-3 font-bold leading-none tracking-tight text-gray-200 md:text-2xl lg:text-xl dark:text-black mt-2">
-            Hive  : <span class="font-extrabold">{{ $hive_id }}</span>
-        </h1>
-      </div>
-
-          <div>
-            <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-                <li class="mr-2">
-                  <a href="{{ url('/hive_data/temperature_data_default/' . $hive_id) }}" class="inline-block px-2 py-2 text-white bg-green-900 rounded-sm active" aria-current="page">Temperature</a>
-                </li>
-                <li class="mr-2">
-                  <a href="{{ url('/hive_data/humidity_data_default/' . $hive_id) }}" class="inline-block px-2 py-2 rounded-sm hover:text-gray-900 hover:bg-lime-200 dark:hover:bg-lime-300 dark:hover:text-black">Humidity</a>
-                </li>
-                {{-- <li class="mr-2">
-                  <a href="{{ url('/hive_data/weight_data_default/' . $hive_id) }}" class="inline-block px-2 py-2 rounded-sm hover:text-gray-900 hover:bg-lime-200 dark:hover:bg-lime-300 dark:hover:text-black">Weight</a>
-                </li>
-                <li class="mr-12">
-                  <a href="{{ url('/hive_data/co2_data_default/' . $hive_id) }}" class="inline-block px-2 py-2 rounded-sm hover:text-gray-900 hover:bg-lime-200 dark:hover:bg-lime-300 dark:hover:text-black">CO<sub>2</sub> levels</a>
-                </li> --}}
-                <li class="mr-2">
-                    <a href="{{ url('admin/hive') }}" class="inline-block px-2 py-2 rounded-sm text-white bg-blue-700 hover:bg-blue-500">Back</a>
-                </li>
-            </ul>          
-
-          </div>
-
-          <div class='mb-2'>
-            <h3 class='mx-3 font-bold py-2'>Select a date-range</h3>
-                <!-- Date picker -->
-            <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #0fc90f; width: 260px" class='mx-3'>
-            <i class="fa fa-calendar"></i>&nbsp;
-            <span></span> <i class="fa fa-caret-down"></i>
-            </div>
-          </div>
-    </div>
+  
 
     <!-- Display the temperature graph -->
     <script type="text/javascript">
@@ -286,7 +263,7 @@ cb(start, end);
 </script>
 
 
-        <div class='bg-blue-400 mx-3 p-1 mt-2'>
+        <div class='bg-white mx-3 p-1 mt-2'>
               <div id="chart" style="width: 100%; height: 480px;" class='p-2'></div>
                 <script>
                 // JavaScript code to create and configure the chart

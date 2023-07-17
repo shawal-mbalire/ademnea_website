17818d0046e91d3d2930d3ffeaf32deb978cd892
Babwat <98084385+Babwat@users.noreply.github.com>
2023-07-17 11:58:12 +0300

    check the scholarship thing

--- resources/views/admin/scholarship/index.blade.php
+++ resources/views/admin/scholarship/index.blade.php
@@ -103,52 +103,78 @@
     </div>
  </div>
  
-    <!-- Edit team member modal -->
-    @foreach($scholarship as $item)
-    <div id="{{ $item->id}}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative w-full max-w-2xl max-h-full">
-            <!-- Modal content -->
-            <form action="/admin/team" accept-charset="UTF-8" enctype="multipart/form-data"   class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <!-- Modal header -->
-                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                        Edit Team Member
-                    </h3>
-                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{ $item->id}}">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-                    </button>
-                </div>
-                <!-- Modal body -->
-                <div class="p-6 space-y-6">
-                    <div class="grid grid-cols-6 gap-6">
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                            <input type="text" name="name" id="name" value="{{ old('name', $item->name) }}" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Bonnie" required="">
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                            <input type="text" name="title" value="{{ old('title', $item->title) }}" id="title" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Green" required="">
-                        </div>
-                        <div class="col-span-6 sm:col-span-6">
-                            <label for="department" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Research Interests</label>
-                            <textarea id="department" name="description" value="{{ old('description', $item->description) }}" rows="4" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Write your thoughts here..."></textarea>
-                        </div>
-                        <div class="col-span-6 sm:col-span-6">
-                            <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image(jpg, peg & png only allowed)</label>
-                            <input class="block w-full text-sm text-gray-900 border shadow-sm bg-gray-50 border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple>
-                        </div>
-                        
-                    </div>
-                </div>
-                <!-- Modal footer -->
-                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                    <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save all</button>
-                </div>
-            </form>
-        </div>
-    </div>
- </div>
- @endforeach
+   
+<!-- Edit scholarships -->
+@foreach($scholarship as $item)
+<div id="{{ $item->id }}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+       <div class="relative w-full max-w-2xl max-h-full">
+           <!-- Modal content -->
+           <form action="{{ url('/admin/scholarship/' . $item->id) }}" method='POST'  accept-charset="UTF-8" enctype="multipart/form-data" method="POST" class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+           {{ method_field('PATCH') }}
+           {{ csrf_field() }}
+               <!-- Modal header -->
+               <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                   <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                       Edit Scholarship
+                   </h3>
+                   <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{ $item->id }}">
+                       <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+                   </button>
+               </div>
+               <!-- Modal body -->
+               <div class="p-6 space-y-6">
+                   <div class="grid grid-cols-6 gap-6">
+                       <div class="col-span-6 sm:col-span-6">  
+                            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a category</label>
+                            <select id="countries" name="category" value="{{old('category')}}"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+                            <option value="masters">Masters</option>
+                            <option value="phd">Phd</option>
+                            </select>
+                       </div>
+                       <div class="col-span-6 sm:col-span-6">  
+                            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a country</label>
+                            <select id="countries" name="country" value="{{old('country')}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+                            <option value="sudan">South Sudan</option>
+                            <option value="uganda">Uganda</option>
+                            <option value="tanzania">Tanzania</option>
+                            </select>
+                       </div>
+
+                       <div class="col-span-6 sm:col-span-6">
+                           <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Instructions</label>
+                           <textarea class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"  rows="5" name="instructions" type="textarea" id="instructions" >{{ old('instructions', $item->instructions) }}</textarea>
+                       </div>
+                       
+                       <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+                        <script>
+                            
+                        CKEDITOR.replace('article', {
+                                filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
+                                filebrowserUploadMethod: 'form',
+                                "extraPlugins" : 'imagebrowser',
+                                "imageBrowser_listUrl" : "/images_list.json"
+                            })
+
+                            CKEDITOR.replace('instructions', {
+                                filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
+                                filebrowserUploadMethod: 'form',
+                                "extraPlugins" : 'imagebrowser',
+                                "imageBrowser_listUrl" : "/images_list.json"
+                            })
+                        </script>
+
+
+                   </div>
+               </div>
+               <!-- Modal footer -->
+               <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                   <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save all</button>
+               </div>
+           </form>
+       </div>
+   </div>
+</div>
+@endforeach
  
  
      <!-- Large Modal -->
@@ -382,77 +408,6 @@
 </div>
 
 
-<!-- Edit scholarships -->
-@foreach($scholarship as $item)
-<div id="{{ $item->id }}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-       <div class="relative w-full max-w-2xl max-h-full">
-           <!-- Modal content -->
-           <form action="{{ url('/admin/scholarship/' . $item->id) }}" method='POST'  accept-charset="UTF-8" enctype="multipart/form-data" method="POST" class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-           {{ method_field('PATCH') }}
-           {{ csrf_field() }}
-               <!-- Modal header -->
-               <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                   <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                       Edit Scholarship
-                   </h3>
-                   <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{ $item->id }}">
-                       <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-                   </button>
-               </div>
-               <!-- Modal body -->
-               <div class="p-6 space-y-6">
-                   <div class="grid grid-cols-6 gap-6">
-                       <div class="col-span-6 sm:col-span-6">  
-                            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a category</label>
-                            <select id="countries" name="category" value="{{old('category')}}"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
-                            <option value="masters">Masters</option>
-                            <option value="phd">Phd</option>
-                            </select>
-                       </div>
-                       <div class="col-span-6 sm:col-span-6">  
-                            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a country</label>
-                            <select id="countries" name="country" value="{{old('country')}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
-                            <option value="sudan">South Sudan</option>
-                            <option value="uganda">Uganda</option>
-                            <option value="tanzania">Tanzania</option>
-                            </select>
-                       </div>
-
-                       <div class="col-span-6 sm:col-span-6">
-                           <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Instructions</label>
-                           <textarea class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"  rows="5" name="instructions" type="textarea" id="instructions" >{{ old('instructions', $item->instructions) }}</textarea>
-                       </div>
-                       
-                       <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
-                        <script>
-                            
-                        CKEDITOR.replace('article', {
-                                filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
-                                filebrowserUploadMethod: 'form',
-                                "extraPlugins" : 'imagebrowser',
-                                "imageBrowser_listUrl" : "/images_list.json"
-                            })
-
-                            CKEDITOR.replace('instructions', {
-                                filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
-                                filebrowserUploadMethod: 'form',
-                                "extraPlugins" : 'imagebrowser',
-                                "imageBrowser_listUrl" : "/images_list.json"
-                            })
-                        </script>
-
-
-                   </div>
-               </div>
-               <!-- Modal footer -->
-               <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                   <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save all</button>
-               </div>
-           </form>
-       </div>
-   </div>
-</div>
-@endforeach
 
 
 

f7a8278ebc59f1540025c99d85dd8f952b47729e
achuka99 <achukasimon1@gmail.com>
2023-07-19 12:31:54 +0300

    added better styles for navbar of visualization pages

--- resources/views/datanavbar.blade.php
+++ resources/views/datanavbar.blade.php
@@ -1,5 +1,5 @@
 
-<div class="flex flex-row space-x-4 items-center justify-center h-16 mb-2 px-10 bg-gray-400 dark:bg-gray-800">
+<div class="flex flex-row mt-10 space-x-4 items-center justify-center h-16 mb-2 px-10 bg-white">
 
 
     {{-- graphs dropdown start --}}
@@ -96,54 +96,3 @@
     </div>
 </div>
 
-
-02
-
-
-<!-- Choose date range -->
-<div class="flex flex-row space-x-4 mt-10 items-center justify-between h-16 mb-4 rounded bg-gray-200 dark:bg-gray-800">
-    <div>
-
-        <h3 class='mx-2 font-bold py-1'>Hive : <span class="font-extrabold">{{ $hive_id }}</span></h3>
-    </div>
-
-    <div>
-        <h3 class='mx-2 font-bold py-1'>Select a date-range</h3>
-        <!-- Date range picker -->
-        <div id="reportrange"
-            style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 250px">
-            <i class="fa fa-calendar"></i>&nbsp;
-            <span></span> <i class="fa fa-caret-down"></i>
-        </div>
-    </div>
-
-    <script type="text/javascript">
-        $(function() {
-
-            var start = moment().subtract(29, 'days');
-            var end = moment();
-
-            function cb(start, end) {
-                $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-            }
-
-            $('#reportrange').daterangepicker({
-                startDate: start,
-                endDate: end,
-                ranges: {
-                    'Today': [moment(), moment()],
-                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                    'This Month': [moment().startOf('month'), moment().endOf('month')],
-                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
-                        'month').endOf('month')]
-                }
-            }, cb);
-
-            cb(start, end);
-
-        });
-    </script>
-</div>
-</div>

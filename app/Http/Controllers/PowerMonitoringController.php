ef9528ebe6711829399bbbda0962eab212c4655f
JenniferBukenya <namyalojenniferb@gmail.com>
2023-07-28 11:37:57 +0300

    This is an initial commit for the power monitoring page

--- app/Http/Controllers/PowerMonitoringController.php
+++ app/Http/Controllers/PowerMonitoringController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 use Response;
 
 
 class PowerMonitoringController extends Controller
 {
     // Controller responsible for displaying newsletters
-    public function powerMonitor()
+    public function powerMonitor($id)
     {
-        return view('admin.power-monitoring.index');
+        $hive_battery = DB::table('hive_battery')->where('hive_id', $id)->get();
+        return view('admin.power-monitoring.index', compact('hive_battery'));
+
     }
     
 }
--- resources/views/admin/power-monitoring/index.blade.php
+++ resources/views/admin/power-monitoring/index.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.app')
 @section('content')
+    @php
+        $hive_id = session('hive_id');
+    @endphp
+    <div>
+        @include('datanavbar')
 
 
-<div>
-    <h1>Power monitoring will be here</h1>
-</div>
+    </div>
+
+    <div>
+        <p>Battery for Hives</p>
 
 
+        @foreach ($hive_battery as $battery)
+            <div class="text-center bold">
+                <b>BATTERY FOR HIVE: {{ $battery->hive_id }}</b>
+            </div>
+
+            <div class="row">
+                <div class="col-4 m-1 donut" style="background-color: aquamarine">
+                    Liquid Battery goes here
+                </div>
+                <div class="col-4 m-1 graph">
+                    Graph goes here
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection

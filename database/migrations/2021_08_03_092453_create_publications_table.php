ed11f5f9db8dbd6c32621769c8c474e27d1a7755
kristianinc <xankiiza@gmail.com>
2023-09-07 15:23:13 +0300

    publication images fixed

--- database/migrations/2021_08_03_092453_create_publications_table.php
+++ database/migrations/2021_08_03_092453_create_publications_table.php
@@ -20,6 +20,7 @@ class CreatePublicationsTable extends Migration
             $table->string('publisher')->nullable();
             $table->string('year')->nullable();
             $table->string('attachment')->nullable();
+            $table->string('image')->nullable();
             });
     }
 
--- resources/views/admin/publication/index.blade.php
+++ resources/views/admin/publication/index.blade.php
@@ -107,8 +107,13 @@
                              });
                          </script>
                     </div>
+
+                    <div class="col-span-6 sm:col-span-6">
+                        <label for="attachment" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ 'Image' }}</label>
+                         <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"  name="image" id="attachment" type="file">
+                         <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help"> (only image allowed)</p>
+                    </div>
                    
-     
                 </div>
             </div>
             <!-- Modal footer -->
@@ -132,7 +137,7 @@
                <!-- Modal header -->
                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                       Add New Publication
+                       Edit Publication
                    </h3>
                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="{{ $item->id}}">
                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
@@ -166,6 +171,12 @@
                                     $("#datepicker").datepicker({dateFormat: 'yyyy'});
                                 });
                             </script>
+                       </div>
+
+                       <div class="col-span-6 sm:col-span-6">
+                        <label for="attachment" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ 'Image' }}</label>
+                         <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"  name="image" id="attachment" type="file">
+                         <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help"> (only image allowed)</p>
                        </div>
                       
         

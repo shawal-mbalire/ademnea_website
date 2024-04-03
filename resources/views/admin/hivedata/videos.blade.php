0188b62c07c1f4ac5d30af812405b1ea44d965c7
aheebwa32 <aheebwasteve3@gmail.com>
2024-04-03 11:31:42 +0300

    video library

--- resources/views/admin/hivedata/videos.blade.php
+++ resources/views/admin/hivedata/videos.blade.php
@@ -1,80 +1,70 @@
 @extends('layouts.app')
 @section('content')
 
-
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-
-@php
-    $hive_id = session('hive_id');
+ @php
+  $hive_id = session('hive_id');
 @endphp
 
 @include('datanavbar')
 
-<div class="relative p-3 mt-10 overflow-x-auto shadow-md sm:rounded-lg">
-    <table id="myTable" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    #
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Hive ID
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Videos
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Date
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        @php
-            $count =  1
-            @endphp
-        @foreach($videos as $video)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {{ $count }}
-                </th>
-                <td class="px-6 py-4">
-                {{ $video->hive_id }}
-                </td>
-                <td class="px-6 py-4">
-                <video width="100px" height="100px"
-                    controls="controls"/>
-                    
-                <source src="{{ URL("hivevideo/"."".$video->path) }}"
-                    type="video/mp4">
-                </video>
-                </td>
-                <td class="px-6 py-4">
-                {{ $video->created_at }}
-                </td>
-            </tr>
-            @php
-                $count = $count + 1
-                @endphp
-            @endforeach  
-        </tbody>
-    </table>
+
+  <div class="container mx-auto flex">
+    <div class="w-2/3">
+        <div class="play-video">
+            <video id="mainVideo" class="w-full" controls>
+                <source src="{{ URL("hivevideo/" . $videos[0]->path) }}" type="video/mp4">
+                
+            </video>
+        </div>
+    </div>
+    <div class="w-1/3 ml-4">
+        <div class="right-sidebar">
+            <h2 class="text-lg font-semibold mb-4">Videos From The Hive</h2>
+            
+            <ul>
+                @foreach($videos as $video)
+                <li class="flex items-center space-x-4 py-2">
+                    <div class="flex-shrink-0">
+                        <a href="#">
+                            {{-- {{ URL("hivevideo/$video->path") }} --}}
+                            <img src="{{ asset('thumbnail/thumbnail.jpeg') }}" data-video="{{ $video->path }}" alt="Video Thumbnail" class="w-16 h-12">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" class="block hover:bg-gray-200 px-2 py-1 rounded playlist-item" data-video="{{ $video->path }}">{{ $video->created_at }}</a>
+                        {{-- <p class="text-xs text-gray-500">{{ $video->created_at }}</p> --}}
+                    </div>
+                </li>
+            @endforeach
+            
+
+            </ul>
+        </div>
+    </div>
 </div>
 
-@endsection
-<!-- added pagination and search-->
-@section('page_scripts')
-<!-- Include DataTables JS file -->
-<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
 
 <script>
-  $(document).ready(function() {
-   $('#myTable').DataTable({
-      responsive: true
-   });
-});
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log("DOM content loaded.");
+        
+        const playlistItems = document.querySelectorAll('.playlist-item');
+
+        playlistItems.forEach(function(item) {
+            item.addEventListener('click', function(event) {
+                event.preventDefault();
+                const videoSource = this.getAttribute('data-video');
+                console.log(videoSource);
+                document.getElementById('mainVideo').src = "{{ URL('hivevideo') }}/" + videoSource;
+                document.getElementById('mainVideo').load();
+            });
+        });
+    });
 </script>
+
 @endsection
+
+@section('page_scripts')
+
+@endsection
+
